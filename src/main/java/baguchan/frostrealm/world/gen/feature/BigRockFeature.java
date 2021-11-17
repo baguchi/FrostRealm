8e6fd63525eb1d1f75f27336b9bef68b35c5a4db
baguchan <shurf012@gmail.com>
2021-11-17 22:13:38 +0900

    fix big rock feature

--- src/main/java/baguchan/frostrealm/world/gen/feature/BigRockFeature.java
+++ src/main/java/baguchan/frostrealm/world/gen/feature/BigRockFeature.java
@@ -3,6 +3,7 @@ package baguchan.frostrealm.world.gen.feature;
 import baguchan.frostrealm.registry.FrostBlocks;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
@@ -36,12 +37,12 @@ public class BigRockFeature extends Feature<BlockStateConfiguration> {
 			return false;
 		} else {
 			int i = random.nextInt(2) + 2;
-			int i2 = random.nextInt(5) + 1;
-			int i3 = random.nextInt(i);
+			int i2 = random.nextInt(2) + 2;
+			int i3 = Mth.clamp(random.nextInt(i), 3, i);
 			float f = (float) (i2 + i + i2) * 0.35F + 0.5F;
 			for (BlockPos blockpos1 : BlockPos.betweenClosed(blockpos.offset(-i2, -i, -i2), blockpos.offset(i2, i, i2))) {
 				if (blockpos1.distSqr(blockpos) <= (double) (f * f)) {
-					worldgenlevel.setBlock(blockpos1.above(i3), blockstateconfiguration.state, 4);
+					worldgenlevel.setBlock(blockpos1.below(i3), blockstateconfiguration.state, 4);
 				}
 
 			}
