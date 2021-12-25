90cd8e8ec9b7d857c1316b8ac525da08e3805eb2
baguchan <shurf012@gmail.com>
2021-12-25 12:14:51 +0900

    fixed stove cannot drop with pickaxe and more hot source

--- src/main/java/baguchan/frostrealm/capability/FrostLivingCapability.java
+++ src/main/java/baguchan/frostrealm/capability/FrostLivingCapability.java
@@ -116,7 +116,7 @@ public class FrostLivingCapability implements ICapabilityProvider, ICapabilitySe
 			if (!entity.getItemBySlot(EquipmentSlot.FEET).isEmpty())
 				tempAffect *= 0.8F;
 			if (entity.isInWaterOrRain())
-				tempAffect *= 2.5F;
+				tempAffect *= 2.0F;
 
 			if (this.hotSource == null) {
 				addExhaustion(tempAffect * 0.001F);
