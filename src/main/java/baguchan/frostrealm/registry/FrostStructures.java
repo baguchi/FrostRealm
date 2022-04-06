f264edbb043d5eefaddf881b47022b1cc271756b
baguchan <shurf012@gmail.com>
2022-04-06 16:31:38 +0900

    readd igloo and delete ruins

--- src/main/java/baguchan/frostrealm/registry/FrostStructures.java
+++ src/main/java/baguchan/frostrealm/registry/FrostStructures.java
@@ -2,7 +2,6 @@ package baguchan.frostrealm.registry;
 
 import baguchan.frostrealm.FrostRealm;
 import baguchan.frostrealm.world.gen.structure.IglooStructure;
-import baguchan.frostrealm.world.gen.structure.RuinsStructure;
 import com.google.common.collect.ImmutableList;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.Holder;
@@ -24,12 +23,8 @@ import java.util.Locale;
 @Mod.EventBusSubscriber(modid = FrostRealm.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class FrostStructures {
 	public static final Holder<StructureTemplatePool> IGLOO_START = Pools.register(new StructureTemplatePool(new ResourceLocation("frostrealm:igloo/igloo_entrance"), new ResourceLocation("empty"), ImmutableList.of(Pair.of(StructurePoolElement.single("frostrealm:igloo/igloo_entrance", ProcessorLists.EMPTY), 1)), StructureTemplatePool.Projection.RIGID));
-	public static final Holder<StructureTemplatePool> RUINS_START = Pools.register(new StructureTemplatePool(new ResourceLocation("frostrealm:ruins/main_ruins"), new ResourceLocation("empty"), ImmutableList.of(Pair.of(StructurePoolElement.single("frostrealm:ruins/main_ruins", ProcessorLists.EMPTY), 1)), StructureTemplatePool.Projection.RIGID));
-
 
 	public static final StructureFeature<JigsawConfiguration> IGLOO = new IglooStructure(JigsawConfiguration.CODEC);
-
-	public static final StructureFeature<JigsawConfiguration> RUINS = new RuinsStructure(JigsawConfiguration.CODEC);
 
 	static StructurePieceType setPieceId(StructurePieceType p_67164_, String p_67165_) {
 		return Registry.register(Registry.STRUCTURE_PIECE, p_67165_.toLowerCase(Locale.ROOT), p_67164_);
@@ -38,7 +33,6 @@ public class FrostStructures {
 	@SubscribeEvent
 	public static void registerStructure(RegistryEvent.Register<StructureFeature<?>> registry) {
 		registry.getRegistry().register(IGLOO.setRegistryName("igloo"));
-		registry.getRegistry().register(RUINS.setRegistryName("ruins"));
 	}
 
 	private static String prefix(String path) {
