20606c94367e9c6a6b1f396b4b58d6c6809019d5
baguchan <shurf012@gmail.com>
2022-06-17 12:44:07 +0900

    add unstable level for purple fog event

--- src/main/java/baguchan/frostrealm/capability/FrostWeatherCapability.java
+++ src/main/java/baguchan/frostrealm/capability/FrostWeatherCapability.java
@@ -73,7 +73,7 @@ public class FrostWeatherCapability implements ICapabilityProvider, ICapabilityS
 					//If weather not active and cooldown active
 					setWeatherCooldown(getWeatherCooldown() - 1);
 					if (getWeatherCooldown() <= 0) {
-						unstableLevel += 0.2F * level.random.nextDouble();
+						unstableLevel += 0.5F * level.random.nextDouble();
 						FrostWeather frostWeather = BlizzardUtils.makeRandomWeather(level.random, this.unstableLevel);
 
 						setFrostWeather(frostWeather);
