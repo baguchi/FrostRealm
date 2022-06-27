75cd35adc7d05ecc8f16b1ae05508bb98b13804b
baguchan <shurf012@gmail.com>
2022-06-28 04:47:36 +0900

    add aurora

--- src/main/java/baguchan/frostrealm/client/FrostRealmRenderInfo.java
+++ src/main/java/baguchan/frostrealm/client/FrostRealmRenderInfo.java
@@ -1,9 +1,11 @@
 package baguchan.frostrealm.client;
 
+import baguchan.frostrealm.client.sky.FrostrealmSkyRenderer;
 import baguchan.frostrealm.client.sky.FrostrealmWeatherParticleRenderer;
 import baguchan.frostrealm.client.sky.FrostrealmWeatherRenderer;
 import net.minecraft.client.renderer.DimensionSpecialEffects;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.client.ISkyRenderHandler;
 import net.minecraftforge.client.IWeatherParticleRenderHandler;
 import net.minecraftforge.client.IWeatherRenderHandler;
 
@@ -12,6 +14,8 @@ import javax.annotation.Nullable;
 public class FrostRealmRenderInfo extends DimensionSpecialEffects {
 	private IWeatherRenderHandler weatherRenderer;
 	private IWeatherParticleRenderHandler weatherParticleRenderHandler = null;
+
+	private ISkyRenderHandler skyRenderHandler = null;
 
 	public FrostRealmRenderInfo(float cloudHeight, boolean placebo, SkyType fogType, boolean brightenLightMap, boolean entityLightingBottomsLit) {
 		super(cloudHeight, placebo, fogType, brightenLightMap, entityLightingBottomsLit);
@@ -41,5 +45,13 @@ public class FrostRealmRenderInfo extends DimensionSpecialEffects {
 		if (weatherParticleRenderHandler == null)
 			weatherParticleRenderHandler = new FrostrealmWeatherParticleRenderer();
 		return weatherParticleRenderHandler;
+	}
+
+	@org.jetbrains.annotations.Nullable
+	@Override
+	public ISkyRenderHandler getSkyRenderHandler() {
+		if (skyRenderHandler == null)
+			skyRenderHandler = new FrostrealmSkyRenderer();
+		return skyRenderHandler;
 	}
 }
