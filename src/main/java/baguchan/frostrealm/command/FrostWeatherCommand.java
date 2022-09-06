06a492b3316caea3ed15592deb8b7c674e6d7d32
baguchan <shurf012@gmail.com>
2022-09-06 22:19:01 +0900

    change weather code bug

--- src/main/java/baguchan/frostrealm/command/FrostWeatherCommand.java
+++ src/main/java/baguchan/frostrealm/command/FrostWeatherCommand.java
@@ -1,6 +1,8 @@
 package baguchan.frostrealm.command;
 
 import baguchan.frostrealm.FrostRealm;
+import baguchan.frostrealm.message.ChangeWeatherEvent;
+import baguchan.frostrealm.message.ChangeWeatherTimeEvent;
 import baguchan.frostrealm.registry.FrostDimensions;
 import baguchan.frostrealm.registry.FrostWeathers;
 import baguchan.frostrealm.weather.FrostWeather;
@@ -13,6 +15,7 @@ import net.minecraft.commands.Commands;
 import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.network.PacketDistributor;
 
 import java.util.Optional;
 import java.util.Set;
@@ -47,7 +50,10 @@ public class FrostWeatherCommand {
 		p_139173_.getLevel().getCapability(FrostRealm.FROST_WEATHER_CAPABILITY).ifPresent(cap -> {
 			cap.setWetherTime(0);
 			cap.setWeatherCooldown(p_139174_);
-			cap.needWeatherCooldownChanged = true;
+
+			ChangeWeatherTimeEvent message = new ChangeWeatherTimeEvent(cap.getWeatherTime(), cap.getWeatherCooldown(), cap.getWeatherLevel(1.0F));
+			FrostRealm.CHANNEL.send(PacketDistributor.ALL.noArg(), message);
+			cap.needWeatherChanged = true;
 		});
 		p_139173_.sendSuccess(Component.translatable("commands.frostrealm.frost_weather.clear"), true);
 
@@ -80,8 +86,13 @@ public class FrostWeatherCommand {
 
 					cap.setWetherTime(p_139179_);
 					cap.setWeatherCooldown(0);
-					cap.needWeatherChanged = true;
 
+					ChangeWeatherEvent message = new ChangeWeatherEvent(frostWeather.get());
+					FrostRealm.CHANNEL.send(PacketDistributor.ALL.noArg(), message);
+
+					ChangeWeatherTimeEvent message2 = new ChangeWeatherTimeEvent(cap.getWeatherTime(), cap.getWeatherCooldown(), cap.getWeatherLevel(1.0F));
+					FrostRealm.CHANNEL.send(PacketDistributor.ALL.noArg(), message2);
+					cap.needWeatherChanged = true;
 				});
 				p_139178_.sendSuccess(Component.translatable("commands.frostrealm.frost_weather.set"), true);
 				return p_139179_;
