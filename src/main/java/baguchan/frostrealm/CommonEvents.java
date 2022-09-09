1dcb99a5987cd990c9d2531755955bf0b2ca9217
baguchan <shurf012@gmail.com>
2022-09-10 08:02:21 +0900

    oops

--- src/main/java/baguchan/frostrealm/CommonEvents.java
+++ src/main/java/baguchan/frostrealm/CommonEvents.java
@@ -6,14 +6,9 @@ import baguchan.frostrealm.message.ChangeWeatherEvent;
 import baguchan.frostrealm.message.ChangeWeatherTimeEvent;
 import baguchan.frostrealm.registry.FrostBlocks;
 import baguchan.frostrealm.registry.FrostDimensions;
-import baguchan.frostrealm.registry.FrostStructures;
-import baguchan.frostrealm.utils.StructureUtils;
-import net.minecraft.core.Registry;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
@@ -21,19 +16,12 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SpawnerBlock;
-import net.minecraft.world.level.block.TorchBlock;
-import net.minecraft.world.level.levelgen.structure.Structure;
-import net.minecraft.world.level.levelgen.structure.StructureStart;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.PacketDistributor;
@@ -129,53 +117,6 @@ public class CommonEvents {
 					p_147232_.broadcastBreakEvent(event.getHand());
 				});
 				event.getLevel().playSound(event.getEntity(), event.getPos(), SoundEvents.HOE_TILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-			}
-		}
-	}
-
-	@SubscribeEvent
-	public static void onBlockBreaked(BlockEvent.BreakEvent event) {
-		if (event.getPlayer() instanceof ServerPlayer) {
-			LevelAccessor world = event.getPlayer().level;
-			if (world instanceof ServerLevel) {
-				ServerLevel serverLevel = (ServerLevel) world;
-				Structure structure = serverLevel.registryAccess().registryOrThrow(Registry.STRUCTURE_REGISTRY).get(FrostStructures.CRYSTAL_TEMPLE);
-				if (structure != null) {
-					StructureStart structureStart = serverLevel.structureManager().getStructureAt(event.getPos(), structure);
-					if (structureStart.isValid()) {
-						if (!StructureUtils.isBeatenDungeons(serverLevel, structureStart.getBoundingBox())) {
-							ServerPlayer player = (ServerPlayer) event.getPlayer();
-							if (!player.isCreative() && !(event.getState().getBlock() instanceof TorchBlock) && !(event.getState().getBlock() instanceof SpawnerBlock)) {
-								player.displayClientMessage(Component.translatable("frostrealm.need_defeat_boss"), true);
-								event.setCanceled(true);
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	@SubscribeEvent
-	public static void onBlockPlaced(BlockEvent.EntityPlaceEvent event) {
-		if (event.getEntity() instanceof ServerPlayer) {
-			LevelAccessor world = event.getEntity().level;
-			if (world instanceof ServerLevel) {
-				ServerLevel serverLevel = (ServerLevel) world;
-				Structure structure = serverLevel.registryAccess().registryOrThrow(Registry.STRUCTURE_REGISTRY).get(FrostStructures.CRYSTAL_TEMPLE);
-				if (structure != null) {
-					StructureStart structureStart = serverLevel.structureManager().getStructureAt(event.getPos(), structure);
-					if (structureStart.isValid()) {
-						if (!StructureUtils.isBeatenDungeons(serverLevel, structureStart.getBoundingBox())) {
-							ServerPlayer player = (ServerPlayer) event.getEntity();
-							Block placedBlock = event.getPlacedBlock().getBlock();
-							if (!player.isCreative() && !(placedBlock instanceof TorchBlock)) {
-								player.displayClientMessage(Component.translatable("frostrealm.need_defeat_boss"), true);
-								event.setCanceled(true);
-							}
-						}
-					}
-				}
 			}
 		}
 	}
