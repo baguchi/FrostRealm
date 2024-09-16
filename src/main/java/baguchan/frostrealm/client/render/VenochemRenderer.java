114868297bba2cf2faa95fab13119253b9230415
baguchan <shutux500@gmail.com>
2024-09-16 15:37:55 +0900

    walk scale change

--- src/main/java/baguchan/frostrealm/client/render/VenochemRenderer.java
+++ src/main/java/baguchan/frostrealm/client/render/VenochemRenderer.java
@@ -7,6 +7,7 @@ import baguchan.frostrealm.entity.hostile.Venochem;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.layers.EyesLayer;
 import net.minecraft.resources.ResourceLocation;
 
 public class VenochemRenderer<T extends Venochem> extends MobRenderer<T, VenochemModel<T>> {
@@ -15,6 +16,12 @@ public class VenochemRenderer<T extends Venochem> extends MobRenderer<T, Venoche
 
     public VenochemRenderer(EntityRendererProvider.Context p_173952_) {
         super(p_173952_, new VenochemModel<>(p_173952_.bakeLayer(FrostModelLayers.VENOCHEM)), 0.5F);
+        this.addLayer(new EyesLayer<>(this) {
+            @Override
+            public RenderType renderType() {
+                return VENOCHEM_GLOW;
+            }
+        });
     }
 
     @Override
