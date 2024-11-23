b8d3b0c110accc8f85a1e2d84faeb222cc328418
baguchan <shutux500@gmail.com>
2024-11-24 06:38:11 +0900

    fluid fix

--- src/main/java/baguchan/frostrealm/CommonEvents.java
+++ src/main/java/baguchan/frostrealm/CommonEvents.java
@@ -31,6 +31,7 @@ import net.minecraft.util.profiling.Profiler;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -53,6 +54,8 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.SelectMusicEvent;
@@ -216,13 +219,57 @@ public class CommonEvents {
             FrostLivingCapability capability = livingEntity.getData(FrostAttachs.FROST_LIVING);
             capability.tick(livingEntity);
             if (livingEntity.isInFluidType(FrostFluidTypes.HOT_SPRING.get())) {
-                FrostFluidTypes.HOT_SPRING.get().move(FrostFluids.HOT_SPRING.get().defaultFluidState(), livingEntity, livingEntity.getDeltaMovement(), livingEntity.getGravity());
+                travelInFluid(livingEntity, livingEntity.getDeltaMovement(), FrostFluids.HOT_SPRING.get().defaultFluidState());
 
                 if (((LivingEntityAccessor) livingEntity).isJump()) {
                     livingEntity.jumpInFluid(FrostFluidTypes.HOT_SPRING.get());
                 }
             }
         }
+    }
+
+    private static void travelInFluid(LivingEntity living, Vec3 p_365480_, FluidState fluidState) {
+        boolean flag = living.getDeltaMovement().y <= 0.0;
+        double d0 = living.getY();
+        double d1 = getEffectiveGravity(living);
+        if (living.isInWater() || (living.isInFluidType(fluidState) && !living.moveInFluid(fluidState, p_365480_, d1))) {
+            float f = living.isSprinting() ? 0.9F : 0.8F;
+            float f1 = 0.02F;
+            float f2 = (float) living.getAttributeValue(Attributes.WATER_MOVEMENT_EFFICIENCY);
+            if (!living.onGround()) {
+                f2 *= 0.5F;
+            }
+
+            if (f2 > 0.0F) {
+                f += (0.54600006F - f) * f2;
+                f1 += (living.getSpeed() - f1) * f2;
+            }
+
+            if (living.hasEffect(MobEffects.DOLPHINS_GRACE)) {
+                f = 0.96F;
+            }
+
+            f1 *= (float) living.getAttributeValue(net.neoforged.neoforge.common.NeoForgeMod.SWIM_SPEED);
+            living.moveRelative(f1, p_365480_);
+            //living.move(MoverType.SELF, living.getDeltaMovement());
+            Vec3 vec3 = living.getDeltaMovement();
+            if (living.horizontalCollision && living.onClimbable()) {
+                vec3 = new Vec3(vec3.x, 0.2, vec3.z);
+            }
+
+            vec3 = vec3.multiply((double) f, 0.8F, (double) f);
+            living.setDeltaMovement(living.getFluidFallingAdjustedMovement(d1, flag, vec3).add(0, d1 / 4, 0));
+        }
+
+        Vec3 vec32 = living.getDeltaMovement();
+        if (living.horizontalCollision && living.isFree(vec32.x, vec32.y + 0.6F - living.getY() + d0, vec32.z)) {
+            living.setDeltaMovement(vec32.x, 0.3F, vec32.z);
+        }
+    }
+
+    protected static double getEffectiveGravity(LivingEntity entity) {
+        boolean flag = entity.getDeltaMovement().y <= 0.0;
+        return flag && entity.hasEffect(MobEffects.SLOW_FALLING) ? Math.min(entity.getGravity(), 0.01) : entity.getGravity();
     }
 
     @SubscribeEvent
--- src/main/java/baguchan/frostrealm/fluidtype/HotSpringFluidType.java
+++ src/main/java/baguchan/frostrealm/fluidtype/HotSpringFluidType.java
@@ -1,64 +1,9 @@
 package baguchan.frostrealm.fluidtype;
 
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.fluids.FluidType;
 
 public class HotSpringFluidType extends FluidType {
     public HotSpringFluidType(FluidType.Properties properties) {
         super(properties);
-    }
-
-    @Override
-    public boolean move(FluidState state, LivingEntity entity, Vec3 movementVector, double gravity) {
-        /*boolean flag = entity.getDeltaMovement().y <= 0.0;
-        float f = entity.isSprinting() ? 0.9F : 0.8F;
-        float f1 = 0.02F;
-        float f2 = (float)entity.getAttributeValue(Attributes.WATER_MOVEMENT_EFFICIENCY);
-        if (!entity.onGround()) {
-            f2 *= 0.5F;
-        }
-
-        if (f2 > 0.0F) {
-            f += (0.54600006F - f) * f2;
-            f1 += (entity.getSpeed() - f1) * f2;
-        }
-
-        if (entity.hasEffect(MobEffects.DOLPHINS_GRACE)) {
-            f = 0.96F;
-        }
-
-        f1 *= (float)entity.getAttributeValue(net.neoforged.neoforge.common.NeoForgeMod.SWIM_SPEED);
-        double d8 = entity.getY();
-
-        entity.moveRelative(f1, movementVector);
-        entity.move(MoverType.SELF, entity.getDeltaMovement());
-
-        if (entity.getFluidTypeHeight(this) <= entity.getFluidJumpThreshold()) {
-            entity.setDeltaMovement(entity.getDeltaMovement().multiply(f, (double) 0.8F, f));
-            Vec3 vec33 = entity.getFluidFallingAdjustedMovement(gravity, flag, entity.getDeltaMovement());
-            entity.setDeltaMovement(vec33);
-        } else {
-            entity.setDeltaMovement(entity.getDeltaMovement().scale(0.85D));
-        }
-
-        *//*if (!entity.isNoGravity()) {
-            entity.setDeltaMovement(entity.getDeltaMovement().add(0.0D, -gravity / 4.0D, 0.0D));
-        }*//*
-
-        Vec3 vec34 = entity.getDeltaMovement();
-        if (entity.horizontalCollision && entity.isFree(vec34.x, vec34.y + (double) 0.6F - entity.getY() + d8, vec34.z)) {
-            entity.setDeltaMovement(vec34.x, (double) 0.3F, vec34.z);
-        }*/
-        entity.addDeltaMovement(new Vec3(0, gravity / 6F, 0));
-
-        return true;
-    }
-
-    protected static double getEffectiveGravity(LivingEntity entity) {
-        boolean flag = entity.getDeltaMovement().y <= 0.0;
-        return flag && entity.hasEffect(MobEffects.SLOW_FALLING) ? Math.min(entity.getGravity(), 0.01) : entity.getGravity();
     }
 }
